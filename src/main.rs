50d19c73fe1e366db0907616217e5de4a1bd1592
RitaMelo18 <aritaamelo@gmail.com>
2025-10-28 10:12:45 +0000

    CWE-1004

--- src/main.rs
+++ src/main.rs
@@ -25,6 +25,7 @@ mod memory_handler;
 mod memory_engine;
 mod directory_handler;
 mod directory_engine;
+mod session_engine;
 
 use crate::asns::*;
 use crate::webservice::*;
@@ -107,4 +108,8 @@ fn main() {
     
     //CWE-90
     let _ = directory_handler::process_directory_stream();
+
+    //CWE-1004
+    let _ = session_engine::poem_cookie_session();
+    let _ = session_engine::warp_with_session();
 }
